5239125c85a018b460396558a9128daac86526d2
zirkonhip-ozerdem <zirkonhip@gmail.com>
2026-08-18 15:47:17 +0300

    Use single Railway pre-deploy command

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,18 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('deploy:prepare', function () {
+    $migrateStatus = $this->call('migrate', [
+        '--force' => true,
+    ]);
+
+    if ($migrateStatus !== 0) {
+        return $migrateStatus;
+    }
+
+    return $this->call('db:seed', [
+        '--class' => \Database\Seeders\AdminUserSeeder::class,
+        '--force' => true,
+    ]);
+})->purpose('Run production migrations and refresh the seeded admin user');
